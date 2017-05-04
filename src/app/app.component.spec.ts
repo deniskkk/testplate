56a5544a64e6d43c49ccbb1bcf690d23d4589d4a
deniskkk <deniskostiuk@gmail.com>
2017-05-04 09:38:06 +0300

    com 21

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,6 +21,6 @@ describe('AppComponent', () => {
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app).not.toBeTruthy();
+    expect(app).toBeTruthy();
   }));
 });
